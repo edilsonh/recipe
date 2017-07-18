1158e2fb6c6b49357ce681628f81236149a3eff0
Edilson Hernandez <edilson.hernandez1@gmail.com>
2017-07-18 19:34:06 -0400

    minor touches here on out

--- main.js
+++ main.js
@@ -1,37 +1,38 @@
 let container = document.querySelector(".food");
-let search = document.getElementById("textInput");
 let button = document.getElementById("submitButton");
-let textVal;
-let url = "http://www.recipepuppy.com/api/?q="
-let fullUrl = "";
-submitButton.addEventListener("click", function(n){
-  textVal = search.value;
-  fullUrl = `${url}${searchItem}`
-  return fullUrl;
-});
+let search = document.getElementById("textInput");
 
-let searchItem = textVal;
-console.log(fullUrl);
-
-
-fetch(fullUrl)
+button.addEventListener("click", function(e){
+  let fullUrl = "https://crossorigin.me/http://www.recipepuppy.com/api";
+  e.preventDefault();
+  fullUrl = `${fullUrl}/?q=${search.value}`;
+  console.log(fullUrl);
+  fetch(fullUrl)
   .then(function(response) {
     if(response.status !== 200) {
       console.log(response.status);
       return;
     }
     response.json().then(function(data) {
-      console.log(data.results[0]);
-      let template =
-      `
-      <section class="stuff">
-        <img src="${data.results[0].thumbnail}">
-        <h1>${data.results[0].title}</h1>
+      let templateHolder = "";
+      data.results.forEach(function(items){
+        console.log(items.title);
+        let template =
+        `
+        <section class="stuff">
+        <img src="${items.thumbnail}" alt="">
+        <h1>${items.title}</h1>
         <h3>Ingredients:</h3>
-        <p>${data.results[0].ingredients}</p>
-        <span><a href="${data.results[0].href}">Check it out</a></span>
-      </section>
-      `
-      container.innerHTML = template;
-    })
-  })
+        <p>${items.ingredients}</p>
+        <span><a href="${items.href}">Check it out</a></span>
+        </section>
+
+        `
+        templateHolder += template;
+      });
+      container.innerHTML = templateHolder;
+      search.value = "";
+
+    });
+  });
+});
